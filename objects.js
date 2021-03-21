2486ea5c4db83dd5ca6f9a48ba2de5a5a70fd449
Rachel <rachelsuk97@gmail.com>
2021-03-21 00:03:15 +0000

    Completed functions on objects.js

--- objects.js
+++ objects.js
@@ -3,11 +3,33 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {};
+  for (const word of phrase.split(' ')) {
+      if (!wordCounts[word]) {
+          wordCounts[word] = 1; 
+      }
+      else {
+          wordCounts[word] += 1; 
+      }
+  }
+  return wordCounts
 }
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  
+  let melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  }
+  
+  if (!melonPrices[price]) {
+    return
+  }
+
+  return melonPrices[price].sort()
+
 }
