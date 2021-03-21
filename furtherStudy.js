bcc75b59b934f8035b24e2f47b18d8dd67652523
Rachel <rachelsuk97@gmail.com>
2021-03-21 00:30:31 +0000

    Completed wordsInCommon function in further study

--- furtherStudy.js
+++ furtherStudy.js
@@ -2,7 +2,20 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+    const words1Set = new Set(words1);
+    console.log(words1Set);
+    const words2Set = new Set(words2);
+    console.log(words2Set);
+
+    let result = new Set();
+
+    for (const word of words1Set) {
+        if (words2Set.has(word)) {
+            console.log('word is in words2set');
+            result.add(word);
+        }
+    }
+    return result;
 }
 
 
